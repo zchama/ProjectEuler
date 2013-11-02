634dc60779c5252c03ce54207ea36a9ae4908229
kj <kj@kajiwara-yasuhiro-no-macbook-pro.local>
2013-11-02 15:16:48 +0900

    Initial Commit

---
+++ PrjectEuler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PrjectEuler
+//
+//  Created by kj on 2013/11/02.
+//  Copyright (c) 2013年 kj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
